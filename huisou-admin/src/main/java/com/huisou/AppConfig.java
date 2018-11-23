e67fc0284b20dc1e00871c3853d5c53af9e82fe6
qinkai <842170861@qq.com>
2018-11-23 18:32:39 +0800

    更新代码

--- huisou-admin/src/main/java/com/huisou/AppConfig.java
+++ huisou-admin/src/main/java/com/huisou/AppConfig.java
@@ -2,7 +2,7 @@ package com.huisou;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.mybatis.spring.annotation.MapperScan;
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
